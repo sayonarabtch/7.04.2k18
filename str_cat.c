782d90c13274ef10fab7240debde4be8e9874cbd
NIKITA <sayonarabtch@gmail.com>
2018-04-07 01:25:30 +0300

    .

---
+++ str_cat.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <string.h>
+int main(){}
+int str_cat() {
+
+	char a[100] = {"Pasha"};
+	char b[100] = {"Technik"};
+	int *pa = &a;
+	int *pb = &b;
+	printf("%s%s \n", *pa, *pb);
+
+}
